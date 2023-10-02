fbcb22b6e890122344db59ae00c1efccdb26e2c4
Qowoeiriir <144234414+Qowoeiriir@users.noreply.github.com>
2023-10-02 14:11:22 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,65 @@
+
+# namber = int(input('Введите число:'))
+# print(namber, namber+1, namber+2, sep='\n')
+#
+# x = int(input('1 число:     '))
+# print(x, x + 1, x + 2, sep='\n')
+
+# a = int(input('длина ребра:  '))
+# v = a ** 3
+# S = 6 * a ** 2
+# print('Объем =', v)
+# print('Площадь полной поверхности =',)
+
+# a_1 = int(input('1 член прогресси '))
+# d = int(input('d '))
+# n = int(input('n '))
+# print(a_1 + d * (n-1))
+
+# n = int(input())
+# print(n //2 * 2 + 2)
+# # 2 -> 4
+# # 3 -> 4
+
+# n = int(input())
+# print('   _~_    ' * n)
+# print('  (o о)   ' * n)
+# print(' /  V  \  ' * n)
+# print('/(  _  )\ ' * n)
+# print(' ^^   ^^  ' * n)
+
+# Часы
+
+# min = abs(int(input('Введите число:  ')))
+#
+# hours = min % (60 * 24) // 60
+# min_last = min % 60
+# print(hours,min_last)
+
+# Вторая цифра справа
+
+# x = int(input('Введите значение'))
+# print(x // 10 % 10)
+
+# # Улитка
+#
+# h = int(input())
+# a = int(input())
+# b = int(input())
+# print((h - a) // (a - b) + 1)
+
+# 0 в 1 и наборот
+
+# x = int(input('Введите число: '))
+# print(1 = x)
+# МКАД
+# speed = int(input('Введите скорость: '))
+# time = int(input('Введите время: '))
+# x = speed * time
+# n = x // 109
+# print(-(109 * n - x))
+
+# Следущее частное
+
+# x = int(input('Ведите значение: '))
+# print((x // 2 + 1) * 2)
